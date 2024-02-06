97b5ce4ef117eeaedfef3cf148c435e6841e1ee0
augustrs <auso0001@stud.kea.dk>
2024-02-06 12:47:55 +0100

    added some CRUD methods to TouristRepository

--- src/main/java/com/example/tourism/repository/TouristRepository.java
+++ src/main/java/com/example/tourism/repository/TouristRepository.java
@@ -11,4 +11,30 @@ import java.util.List;
 public class TouristRepository {
     List<TouristAttraction> attractions = new ArrayList<>();
 
+
+
+    public TouristAttraction createTouristAttraction(String name, String description) {
+        TouristAttraction touristAttraction = new TouristAttraction(name,description);
+        return touristAttraction;
+    }
+    public void addTouristAttraction(TouristAttraction touristAttraction) {
+        attractions.add(touristAttraction);
+    }
+    public void deleteTouristAttractionFromList(String touristAttractionNameString) {
+
+        if (searchForTouristAttraction(touristAttractionNameString).size() == 1) {
+            attractions.remove(searchForTouristAttraction(touristAttractionNameString).get(0));
+
+        }
+        // TODO add for multiple searchresults
+    }
+    public List<TouristAttraction> searchForTouristAttraction(String attractionSearchKey) {
+        List<TouristAttraction> searchResult = new ArrayList<>();
+        for (TouristAttraction attraction : attractions) {
+            if (attraction.getName().toLowerCase().contains(attractionSearchKey.toLowerCase())) {
+                searchResult.add(attraction);
+            }
+        }
+        return searchResult;
+    }
 }
---
+++ src/main/java/com/example/tourism/service/TouristService.java
@@ -0,0 +1,8 @@
+package com.example.tourism.service;
+
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class TouristService {
+}
